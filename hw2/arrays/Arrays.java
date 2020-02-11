e630b6378f08ad8c7f7bc85cc11e1f4e48c1746d
Jenny Mei <jennymei@berkeley.edu>
2020-02-10 21:08:14 -0800

    Finished arrays

--- hw2/arrays/Arrays.java
+++ hw2/arrays/Arrays.java
@@ -6,7 +6,7 @@ package arrays;
 /** HW #2 */
 
 /** Array utilities.
- *  @author
+ *  @author Jenny Mei
  */
 class Arrays {
 
@@ -14,16 +14,38 @@ class Arrays {
     /** Returns a new array consisting of the elements of A followed by the
      *  the elements of B. */
     static int[] catenate(int[] A, int[] B) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] result = new int[A.length + B.length];
+        for (int a = 0; a < A.length; a += 1) {
+            result[a] = A[a];
+        }
+        for (int b = 0; b < B.length; b += 1) {
+            result[A.length + b] = B[b];
+        }
+        return result;
     }
 
     /* C2. */
     /** Returns the array formed by removing LEN items from A,
      *  beginning with item #START. */
     static int[] remove(int[] A, int start, int len) {
-        /* *Replace this body with the solution. */
-        return null;
+        int[] result = new int[A.length - len];
+        int current = 0;
+        while (start != 0) {
+            result[current] = A[current];
+            current += 1;
+            start -= 1;
+        }
+        int stop = current;
+        while (len != 0) {
+            current += 1;
+            len -= 1;
+        }
+        while (current < A.length) {
+            result[stop] = A[current];
+            current += 1;
+            stop += 1;
+        }
+        return result;
     }
 
     /* C3. */
@@ -33,7 +55,30 @@ class Arrays {
      *  returns the three-element array
      *  {{1, 3, 7}, {5}, {4, 6, 9, 10}}. */
     static int[][] naturalRuns(int[] A) {
-        /* *Replace this body with the solution. */
-        return null;
+        int increases = 1;
+        for (int i = 0; i < A.length - 1; i += 1) {
+            if (A[i] > A[i + 1]) {
+                increases += 1;
+            }
+        }
+
+        int[][] result = new int[increases][];
+        int[] entry = {};
+        int current = 0;
+        for (int i = 0; i <= A.length; i += 1) {
+            if (i == 0) {
+                entry = new int[] {A[0]};
+            } else if (i == A.length) {
+                result[current] = entry;
+            } else if (A[i] < A[i - 1]) {
+                result[current] = entry;
+                current += 1;
+                entry = new int[] {A[i]};
+            } else {
+                entry = catenate(entry, new int[] {A[i]});
+            }
+        }
+
+        return result;
     }
 }
--- hw2/arrays/ArraysTest.java
+++ hw2/arrays/ArraysTest.java
@@ -3,13 +3,46 @@ package arrays;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-/** FIXME
- *  @author FIXME
+/** Tests for the Arrays class.
+ *  @author Jenny Mei
  */
 
 public class ArraysTest {
-    /** FIXME
-     */
+    @Test
+    public void catenateTest() {
+        int[] l1 = {1, 2, 3, 4, 5};
+        int[] l2 = {6, 7, 8, 9, 10};
+        int[] l3 = {};
+        int[] l4 = {1};
+
+        assertArrayEquals(new int[] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}, Arrays.catenate(l1, l2));
+        assertArrayEquals(new int[] {1}, Arrays.catenate(l3, l4));
+        assertArrayEquals(new int[] {1, 6, 7, 8, 9, 10}, Arrays.catenate(l4, l2));
+    }
+
+    @Test
+    public void removeTest() {
+        int[] l1 = {1, 2, 3, 4, 5, 6};
+        int[] l2 = {};
+        int[] l3 = {6, 1};
+
+        assertArrayEquals(new int[] {1, 2, 5, 6}, Arrays.remove(l1, 2, 2));
+        assertArrayEquals(new int[] {}, Arrays.remove(l2, 0, 0));
+        assertArrayEquals(new int[] {}, Arrays.remove(l3, 0, 2));
+    }
+
+    @Test
+    public void naturalRunsTest() {
+        int[] l1 = {3, 4, 6, 1, 5, 2, 7, 8, 9};
+        int[] l2 = {};
+        int[] l3 = {5, 4, 3, 2, 1};
+
+        int[][] ll1 = {{3, 4, 6}, {1, 5}, {2, 7, 8, 9}};
+        int[][] ll2 = {{}};
+        int[][] ll3 = {{5}, {4}, {3}, {2}, {1}};
+
+        assertTrue(Utils.equals(ll1, Arrays.naturalRuns(l1)));
+    }
 
     public static void main(String[] args) {
         System.exit(ucb.junit.textui.runClasses(ArraysTest.class));
