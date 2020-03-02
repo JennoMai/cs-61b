d3ccebca4261fa876d683664bb5e0492543c048e
Jenny Mei <jennymei@berkeley.edu>
2020-03-02 15:47:24 -0800

    fixed machinetest

--- proj1/enigma/MachineTest.java
+++ proj1/enigma/MachineTest.java
@@ -17,8 +17,8 @@ public class MachineTest {
         cNAVALA.add(new MovingRotor("I", new Permutation(NAVALA.get("I"), UPPER), ""));
         cNAVALA.add(new MovingRotor("IV", new Permutation(NAVALA.get("IV"), UPPER), "J"));
         cNAVALA.add(new MovingRotor("III", new Permutation(NAVALA.get("III"), UPPER), "V"));
-        cNAVALA.add(new MovingRotor("Beta", new Permutation(NAVALA.get("Beta"), UPPER), ""));
-        cNAVALA.add(new MovingRotor("B", new Permutation(NAVALA.get("B"), UPPER), ""));
+        cNAVALA.add(new FixedRotor("Beta", new Permutation(NAVALA.get("Beta"), UPPER)));
+        cNAVALA.add(new Reflector("B", new Permutation(NAVALA.get("B"), UPPER)));
     }
 
     @Test
