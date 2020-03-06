70d19149b2cf17e14888d944761a0aaca8e662f7
Jenny Mei <jennymei@berkeley.edu>
2020-03-05 22:59:15 -0800

    finished hw4

--- hw4/P2Pattern.java
+++ hw4/P2Pattern.java
@@ -4,18 +4,18 @@
 
 public class P2Pattern {
     /* Pattern to match a valid date of the form MM/DD/YYYY. Eg: 9/22/2019 */
-    public static final String P1 = ""; //FIXME: Add your regex here
+    public static final String P1 = "(\\d|0\\d|1[0-2])/(\\d|0\\d|1\\d|2\\d|3[01])/(19\\d\\d|[2-9]\\d\\d\\d)"; //FIXME: Add your regex here
 
     /** Pattern to match 61b notation for literal IntLists. */
-    public static final String P2 = ""; //FIXME: Add your regex here
+    public static final String P2 = "\\((\\d+,\\s+)*(\\d+)\\)"; //FIXME: Add your regex here
 
     /* Pattern to match a valid domain name. Eg: www.support.facebook-login.com */
-    public static final String P3 = ""; //FIXME: Add your regex here
+    public static final String P3 = "^([^-.\\s_]|[^-.\\s_]([^\\s_])*[^-.\\s_])\\.[^\\s_]{2,5}[^-.\\s_]$"; //FIXME: Add your regex here
 
     /* Pattern to match a valid java variable name. Eg: _child13$ */
-    public static final String P4 = ""; //FIXME: Add your regex here
+    public static final String P4 = "^[^\\d].*"; //FIXME: Add your regex here
 
     /* Pattern to match a valid IPv4 address. Eg: 127.0.0.1 */
-    public static final String P5 = ""; //FIXME: Add your regex here
+    public static final String P5 = "(\\d|\\d\\d|[0-1]\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|\\d\\d|[0-1]\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|\\d\\d|[0-1]\\d\\d|2[0-4]\\d|25[0-5])\\.(\\d|\\d\\d|[0-1]\\d\\d|2[0-4]\\d|25[0-5])$"; //FIXME: Add your regex here
 
 }
--- hw4/ReadInts.java
+++ hw4/ReadInts.java
@@ -31,7 +31,7 @@ public class ReadInts {
         Scanner s = new Scanner(input);
         while (s.hasNext()) {
             int nextInt = s.nextInt();
-            // FIXME
+            L.add(nextInt);
         }
         return L;
     }
@@ -44,7 +44,18 @@ public class ReadInts {
      *  Use the hasNext(), next(), nextInt() and hasNextInt() methods
      *  on Scanners. */
     public static List<Integer> smartReadInts(String input) {
-        // FIXME
-        return null;
+        ArrayList<Integer> L = new ArrayList<>();
+
+        Scanner s = new Scanner(input);
+        while (s.hasNext()) {
+            if (s.hasNextInt()) {
+                int nextInt = s.nextInt();
+                L.add(nextInt);
+            } else {
+                s.next();
+            }
+        }
+
+        return L;
     }
 }
