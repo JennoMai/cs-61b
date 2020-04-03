264e13bc9c86abb640ca8062a5c9bdf99eec4cce
Jenny Mei <jennymei@berkeley.edu>
2020-04-03 02:54:29 -0700

    removed test oops

--- proj2/loa/Board.java
+++ proj2/loa/Board.java
@@ -39,8 +39,7 @@ class Board {
 
     /** A new board in the standard initial position. */
     Board() {
-        this(TEST, BP);
-//        this(INITIAL_PIECES, BP);
+        this(INITIAL_PIECES, BP);
     }
 
     /** A Board whose initial contents and state are copied from
