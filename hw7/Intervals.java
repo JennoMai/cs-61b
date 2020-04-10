b2b036fa39be0f5685ef7714af815b7e85347ed6
Jenny Mei <jennymei@berkeley.edu>
2020-04-10 14:02:22 -0700

    fixed

--- hw7/Intervals.java
+++ hw7/Intervals.java
@@ -1,9 +1,7 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
 
 /** HW #7, Sorting ranges.
  *  @author Jenny Mei
@@ -14,44 +12,11 @@ public class Intervals {
      *  total length covered by the union of the intervals. */
     public static int coveredLength(List<int[]> intervals) {
         // REPLACE WITH APPROPRIATE STATEMENTS.
-        int k = intervals.size();
-        int[][] sorted = new int[k][];
+        intervals.sort(Comparator.comparingInt(t -> t[0]));
 
-        int divisor = 1;
-        int max = 0;
-        for (int i = 0; i < k; i += 1) {
-            sorted[i] = intervals.get(i);
-            if (intervals.get(i)[0] > max) {
-                max = intervals.get(i)[0];
-            }
-        }
-        while (Math.floorDiv(max, divisor) > 0) {
-            int[] count = new int[10];
-            Arrays.fill(count, 0);
-            for (int i = 0; i < k; i += 1) {
-                count[Math.floorDiv(sorted[i][0], divisor) % 10] += 1;
-            }
-            for (int i = 1; i < 10; i += 1) {
-                count[i] += count[i - 1];
-            }
-            int[][] result = new int[k][];
-            for (int i = k - 1; i >= 0; i -= 1) {
-                int digit = Math.floorDiv(sorted[i][0], divisor) % 10;
-                int endIndex = count[digit];
-                result[endIndex - 1] = sorted[i];
-                count[digit] -= 1;
-            }
-
-            for (int i = 0; i < k; i += 1) {
-                sorted[i] = result[i];
-            }
-
-            divisor *= 10;
-        }
-
-        int length = sorted[0][1] - sorted[0][0];
-        int largest = sorted[0][1];
-        for (int[] interval : sorted) {
+        int length = intervals.get(0)[1] - intervals.get(0)[0];
+        int largest = intervals.get(0)[1];
+        for (int[] interval : intervals) {
             if (interval[0] > largest) {
                 largest = interval[1];
                 length += largest - interval[0];
